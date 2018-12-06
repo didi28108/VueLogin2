3cd34cf8614cf6877dd8b5841a315b6c3d7e4748
Philips Igniz Alexander <didi2810812@gmail.com>
2018-12-06 20:01:50 +0800

    Update the axios connect to the control server

--- src/components/Workstation.vue
+++ src/components/Workstation.vue
@@ -3,7 +3,7 @@
     <el-col :span="12">
       <div class="block">
         <el-card :body-style="{ padding: '0px' } " class="webcam">
-            <img src="@/assets/logo.png" class="image">
+            <img v-bind:src="streamUrl" class="image">
             <div style="padding: 14px;">
                 <span>實時監控</span>
                 <div class="bottom clearfix">
@@ -19,17 +19,18 @@
           </div>
           <div>
             <span>x 軸</span>
-            <el-slider v-model="xAxisStep" show-input max="5000"></el-slider>
+            <el-slider v-model="xAxisStep" show-input :max=5000></el-slider>
           </div>
           <div>
             <span>y 軸</span>
-            <el-slider v-model="yAxisStep" show-input max="5000"></el-slider>
+            <el-slider v-model="yAxisStep" show-input :max=5000></el-slider>
           </div>
           <div>
             <span>z 軸</span>
-            <el-slider v-model="zAxisStep" show-input max="5000"></el-slider>
+            <el-slider v-model="zAxisStep" show-input :max=5000></el-slider>
           </div>
-          <el-button class ="button" type="primary" v-on:click="null">執行</el-button>
+          <el-button class ="button" type="primary" v-on:click="goForward">前進</el-button>
+          <el-button class ="button" type="primary" v-on:click="goBackward">後退</el-button>
         </el-card>
       </div>
       <div>
@@ -39,13 +40,14 @@
           </div>
           <div>
             <span>澆水</span>
-            <el-slider v-model="waterpump" :step="1" show-stops max="10"></el-slider>
+            <el-slider v-model="waterpump" :step=1 show-stops :max=10></el-slider>
           </div>
+          <el-button class ="button" type="primary" v-on:click="controlWaterPump">澆水</el-button>
           <div>
             <span>施肥</span>
-            <el-slider v-model="fertillizerpump" :step="1" show-stops max="10"></el-slider>
+            <el-slider v-model="fertilizerpump" :step=1 show-stops :max=10></el-slider>
           </div>
-          <el-button class ="button" type="primary" v-on:click="null">執行</el-button>
+          <el-button class ="button" type="primary" v-on:click="controlFertilizerPump">施肥</el-button>
         </el-card>
       </div>
       <div>
@@ -54,7 +56,7 @@
             <span>LED光色</span>
           </div>
           <el-color-picker v-model="LEDColor"  :predefine="predefineColors"></el-color-picker>
-          <el-button class ="button" type="primary" v-on:click="null">執行</el-button>
+          <el-button class ="button" type="primary" v-on:click="ChangeLEDState">執行</el-button>
         </el-card>  
       </div>
     </el-col>
@@ -68,9 +70,10 @@ export default {
   name: "dashboard",
   data: function() {
     return {
-      xAxisStep: "",
-      yAxisStep: "",
-      zAxisStep: "",
+      streamUrl: "",
+      xAxisStep: 0,
+      yAxisStep: 0,
+      zAxisStep: 0,
       LEDColor: '#409EFF',
       predefineColors: [
           '#ff4500',
@@ -80,37 +83,54 @@ export default {
           '#00ced1',
           '#1e90ff',
           '#c71585',
-        ],
-      waterpump: "",
-      fertillizerpump: "",
+      ],
+      waterpump: 0,
+      fertilizerpump: 0,
       currentDate: ""
     };
   },
   created: function() {
-    this.da = "loading";
-    var self = this;
-    self.getData();
-    setInterval(() => {
-      self.getData();
-    }, 10000);
+    
   },
   methods: {
-    getData: function() {
-      var self = this;
+    axiosGetRequest: function(requestUrl){
+      let self = this;
       axios
-        .get("http://localhost:5438/data")
+        .get(requestUrl)
         .then(resp => {
-          self.humidity = resp.data.humidity;
-          self.temperture = resp.data.temperture;
-          self.soild1humid1 = resp.data.soild1humid1;
-          self.soild1humid2 = resp.data.soild1humid1;
-          self.ph = resp.data.ph;
-          self.waterLevelTank1 = resp.data.waterLevelTank1;
-          self.waterLevelTank2 = resp.data.waterLevelTank2;
+          console.log(`job added:${requestUrl}`);
         })
         .catch(error => {
           console.log(error);
         });
+    },
+    goForward: function(){
+      let self = this;
+      let goForwardUrl = `http://127.0.0.1:10423/method/goPoint?x=${self.xAxisStep}&y=${self.yAxisStep}&z=${self.zAxisStep}`;
+      self.axiosGetRequest(goForwardUrl);
+    },
+    goBackward: function(){
+      let self = this;
+      let backForwardUrl = `http://127.0.0.1:10423/method/backPoint?x=${self.xAxisStep}&y=${self.yAxisStep}&z=${self.zAxisStep}`;
+      self.axiosGetRequest(backForwardUrl);
+    },
+    controlWaterPump: function(){
+      let self = this;
+      let waterSecond = 1000* self.waterpump;
+      let waterPumpUrl = `http://127.0.0.1:10423/method/controlWaterPump?state=true&time=${waterSecond}`;
+      self.axiosGetRequest(waterPumpUrl);
+    },
+    controlFertilizerPump: function(){
+      let self = this;
+      let fertilizerSecond = 1000* self.fertilizerpump;
+      let fertilizerPumpUrl = `http://127.0.0.1:10423/method/controlFertilizerPump?state=true&time=${fertilizerSecond}`;
+      self.axiosGetRequest(fertilizerPumpUrl);
+    },
+    ChangeLEDState: function(){
+      let self = this;
+      let LEDColorFormat = self.LEDColor.slice(1);
+      let LEDUrl = `http://127.0.0.1:10423/method/changeLedState?color=${LEDColorFormat}`
+      self.axiosGetRequest(LEDUrl);
     }
   },
   components: {
