dce619a04d2999346f4d984976b423acea00e5e0
Philips Igniz Alexander <didi2810812@gmail.com>
2018-12-27 04:21:10 +0800

    Add histView template

--- src/components/HistoryView.vue
+++ src/components/HistoryView.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -10,7 +28,8 @@ export default {
   name: 'historyView',
   data () {
     return {
-      msg: 'HistoryView'
+      msg: 'HistoryView',
+      tableData: [],
     }
   },
   created: function() {
